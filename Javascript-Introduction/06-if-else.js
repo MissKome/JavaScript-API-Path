e89b73feae5cdfd3146e1fe8d133f1d72f85aa7d
Oghenekome Edaware <edawarekome@gmail.com>
2023-08-12 21:46:11 +0100

    if-else

---
+++ Javascript-Introduction/06-if-else.js
@@ -0,0 +1,16 @@
+let attendance = 11;
+
+// if (attendance === 15) {
+//     console.log ("We are full capacity")
+// } else {
+//     console.log ("We have room ")
+// }
+
+
+if (attendance < 15) {
+    console.log ("we have room ")
+}else if (attendance > 15) {
+    console.log ("we are over capacity")
+}else {
+    console.log ("We are full capacity")
+}
