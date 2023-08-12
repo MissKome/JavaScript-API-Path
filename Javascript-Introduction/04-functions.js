c8ce0632de9c84b8f9d1b7b11f984d0bb1b96835
Oghenekome Edaware <edawarekome@gmail.com>
2023-08-12 21:00:11 +0100

    Functions

---
+++ Javascript-Introduction/04-functions.js
@@ -0,0 +1,17 @@
+//Sum
+function sum (a,b) {
+   return a + b;
+}
+
+console.log(sum(10,10));
+
+//Product
+function product (a,b) {
+   return a * b;
+}
+
+console.log(product(10,10));
+
+const bigProduct = product(122, 1234);
+
+console.log(bigProduct);
