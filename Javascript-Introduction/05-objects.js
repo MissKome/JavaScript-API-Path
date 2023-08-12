716880a6a0a2bba34bca8769544692ae5ff6de3d
Oghenekome Edaware <edawarekome@gmail.com>
2023-08-12 21:37:05 +0100

    Objects,  Classes

--- Javascript-Introduction/05-objects.js
+++ Javascript-Introduction/05-objects.js
@@ -1,8 +1,20 @@
 const food = {
     name: "chicken and chip",
     isSweet: true
-};
-
+}  
 food.name = "rice";
 console.log(food)
-console.log(food.name) // to see only a name property
+console.log(food.name) // to see only the name property
+
+
+
+//Classes
+class Food {
+    constructor(name, isSweet) {
+this.name = name;
+this.isSweet = isSweet;
+    }
+}
+const myFood = new Food("chicken", true);
+
+console.log(myFood)
